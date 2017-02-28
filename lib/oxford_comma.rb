00828671043fb29d91962285b2ce95b0738eef44
Duncan Maina <duncandevs@gmail.com>
2017-02-28 16:51:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  length = array.size
+  if length == 1
+    return array[0]
+  elsif length == 2
+    return array.join(" and ")
+  else
+    lastel = array.pop
+    str = array.join(", ")
+    str << ", and #{lastel}"
+  end
 end
